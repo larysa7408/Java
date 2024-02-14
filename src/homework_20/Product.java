bfd307ea9f562360fd46ab8bf7f18da517294f08
larysa7408 <larisa7408tema@gmail.com>
2024-02-14 17:17:44 +0100

    Task.java

--- src/homework_20/Product.java
+++ src/homework_20/Product.java
@@ -1,8 +1,8 @@
 package homework_20;
 
 public class Product {
-   String name;
-     Double price;
+    private String name;
+    private Double price;
 
     public Product(String name, Double price) {
         this.name = name;
@@ -17,12 +17,12 @@ public class Product {
         this.price = newPrice;
     }
 
-    public void inStock(){
+    public void inStock() {
         System.out.println("Товар: " + name + ", в наличии");
     }
 
-    public void notStock(){
-        System.out.println("Упс!!! Товар: "+ name + " к сожалению, нет в наличии");
+    public void notStock() {
+        System.out.println("Упс!!! Товар: " + name + " к сожалению, нет в наличии");
     }
 
     public String toString() {
