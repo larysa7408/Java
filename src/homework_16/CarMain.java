89238a1abb7a3d10e39352e539bd0908d5adc479
Larysa Petrova <158000446+larysa7408@users.noreply.github.com>
2024-02-21 19:36:30 +0100

    Task.java

--- src/homework_16/CarMain.java
+++ src/homework_16/CarMain.java
@@ -2,8 +2,8 @@ package homework_16;
 
 public class CarMain {
     public static void main(String[] args) {
-        Car car = new Car("Audi", 100);
-        PetrolStation petrolStation = new PetrolStation(100);
+        Car car = new Car("Audi", 60);
+        PetrolStation petrolStation = new PetrolStation(1000);
         petrolStation.refuelCar(car, 100);
         System.out.println("Остаток топлива на заправке :" + petrolStation.fuelStock);
         System.out.println(" Остаток топлива в автомобиле :" + car.model + petrolStation.fuelStock);
